9ad0f287df1d2bfb16cd973198a486327db52c09
Mazen elziny <you@example.co>
2025-02-28 22:32:44 +0200

    Hello Github

---
+++ main 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char names  [100] ;
+printf ("enter your name:");
+scanf(" %s", &names);
+printf("********************\n");
+printf("Congrat Name %s", &names);
+
+
+    return 0;
+}
